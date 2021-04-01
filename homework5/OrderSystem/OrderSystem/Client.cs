088bac462870d4445d16a56f530466043672067e
sakuraohayo <68192194+sakuraohayo@users.noreply.github.com>
2021-04-01 16:31:22 +0800

    Add files via upload

---
+++ homework5/OrderSystem/OrderSystem/Client.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OrderSystem
+{
+    class Client
+    {
+        public string _clientName { set; get; }
+        public Client(string name)
+        {
+            //设置客户姓名
+            this._clientName = name;
+            ClientName = name;
+        }
+        public string ClientName { set; get; }
+        public override string ToString()
+        {
+            return "clientName:" + _clientName;
+        }
+    }
+}
---
+++ homework5/OrderSystem/OrderSystem/Goods.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OrderSystem
+{
+    class Goods
+    {
+        private string _goodsName { set; get; }
+        private double _goodsPrice { set; get; }
+        public Goods(string name, double price)
+        {
+            //设置商品名称和价格
+            this._goodsName = name;
+            GoodsName = name;
+            this._goodsPrice = price;
+            GoodsPrice = price;
+        }
+        public string GoodsName { set; get; }
+        public double GoodsPrice { set; get; }
+        public override string ToString()
+        {
+            return "goodsName:" + _goodsName + " goodsPrice:" + _goodsPrice;
+        }
+    }
+}
+
